91ba63e820e27daed2c41e28542fa98f24ffae37
RuchikaAmbat <101166813+RuchikaAmbat@users.noreply.github.com>
2022-06-03 14:56:42 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Here goes the statements used by the program
+        System.out.println("Hello world!");
+    }
+}
+
